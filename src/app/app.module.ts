ffd64afcb190c597f604f3a7845f5976fe031659
Kirill Makharadze <kirill.madze@gmail.com>
2020-05-20 18:23:59 +0300

    Manage employers form created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ManageEmployersFormModule } from '../manage-employers-form/manage-employers-form.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ManageEmployersFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
